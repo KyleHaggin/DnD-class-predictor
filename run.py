615e824c6df932eb98147f50220b63c22154e7a4
KyleHaggin <kylelhaggin@gmail.com>
2019-09-26 23:40:34 -0700

    Formatted margins on Predicitons page to allow for easier use. Updated contact info.

--- run.py
+++ run.py
@@ -45,8 +45,8 @@ footer = dbc.Container(
             html.P(
                 [
                     html.Span('Your Name', className='mr-2'), 
-                    html.A(html.I(className='fas fa-envelope-square mr-1'), href='mailto:<you>@<provider>.com'), 
-                    html.A(html.I(className='fab fa-github-square mr-1'), href='https://github.com/<you>/<repo>'), 
+                    html.A(html.I(className='fas fa-envelope-square mr-1'), href='mailto:kylelhaggin@gmail.com'), 
+                    html.A(html.I(className='fab fa-github-square mr-1'), href='https://github.com/KyleHaggin/DnD-class-predictor'), 
                 ], 
                 className='lead'
             )
